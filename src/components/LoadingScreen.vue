4d8cb246cc827460df6240df6d570bc7da920b28
alekcmirniy <alekcmirniy@bk.ru>
2025-10-10 19:42:48 +0300

    started loading screen component realising

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="screen-wrapper">
+        <img src="../components/icons/Loading.gif" alt="Загрузка..." fetchpriority="high" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    data() {
+        return {
+                //ее надо растянуть по всему экрану и сделать непрозрачной??
+        }
+    }
+})
+</script>
+
+<style scoped lang="scss">
+.screen-wrapper {
+    background: white;
+    img {
+        z-index: 1;
+    }
+}
+
+
+</style>
